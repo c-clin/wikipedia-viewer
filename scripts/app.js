88e13cda5721ac93531c94e9b25cf741b719e84a
Catherine Lin <catherine.lin.93@gmail.com>
2018-05-17 18:50:49 -0700

    search button works

--- scripts/app.js
+++ scripts/app.js
@@ -5,25 +5,34 @@ $(document).ready(function () {
 
     $('.search-btn').click(() => {
         searchWiki();
+        $("#search").val("");
     });
 
     $('.lucky-btn').click(() => {
         searchLucky();
+        $('#search').val('');
     });
 
-    
+    // search wiki that returns 10 results
     function searchWiki() {
         let query = $('#search').val();
-        let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&limit=10&namespace=0&redirects=return&format=json&callback=?';
+        let numOfResults = 10;
+        let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${query}&limit=${numOfResults}&namespace=0&redirects=return&format=json&callback=?`;
         $.getJSON(url)
             .done((data) => {
                 console.log(data);
+                for(let i = 0; i < numOfResults; i++) {
+                    titles[i] = data[1][i];
+                    summary[i] = data[2][i];
+                    urls[i] = data[3][i];
+                }
             })
             .fail((error) => {
-                console.log('fail(): ' + error);
+                alert('fail(): ' + error);
             })
     };
 
+    // opens the first search on feeling lucky
     function searchLucky() {
         let query = $("#search").val();
         let url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + query + '&limit=1&namespace=0&redirects=return&format=json&callback=?';
@@ -34,7 +43,7 @@ $(document).ready(function () {
                 window.open(luckyUrl);
             })
             .fail((error) => {
-                console.log('fail(): ' + error);
+                alert('fail(): ' + error);
             })
     }
 
